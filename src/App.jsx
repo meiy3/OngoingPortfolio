7e8dedb4ee8a931afe445731cadd8a723e3e16aa
meiy3 <demandantem5@gmail.com>
2025-04-01 20:11:02 +0800

    changed router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import './styles.css'
-import NavBar from './NavBar.jsx'
-import { Routes, Route } from 'react-router-dom'
-import Home from './Page/Home.jsx'
+import React from 'react';
+import './styles.css';
+import NavBar from './NavBar.jsx';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Page/Home.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className=''>
-      <NavBar />
+    <Router>
       <div>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
-    </div>
+    </Router>
   );
-};
+}
 
-export default App
+export default App;
